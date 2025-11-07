65086c5a50589986238c40f420baaa190be84bbb
R-Vilela <raulvilela29@gmail.com>
2025-11-07 17:28:23 -0400

    feat(doubleList): add funcions insert, searchm and not finished dell

--- doubleList.c
+++ doubleList.c
@@ -6,8 +6,39 @@ void create(DoubleList *list){
     list->tail = NULL;
 }
 
-int insert(DoubleList *list, DataType data);
+int insert(DoubleList *list, DataType data){
+    Node *n = (Node *) malloc(sizeof(Node));
+    n->info = data;
+    if(n==NULL)
+        return 0;
+    if(list->tail==NULL){
+        list->tail = n;
+        list->head = n;
+        n->next = list->head;
+        n->prev = list->tail;
+    }else{
+        n->next = list->head;
+        list->head->prev = n;
+        list->head = n;
+    }
+    return 1;
+};
 
-int dell(DoubleList *list, DataType data);
+int dell(DoubleList *list, DataType data){
+    Node *n;
 
-DataType const * search(DoubleList *list, DataType data);
+
+}
+
+DataType const * search(DoubleList *list, DataType data){
+    Node *n;
+
+    for(n = list->head; !comp(data, n->info)&&comp(list->tail->info, n->info);n = n->next);
+
+    if(comp(list->tail->info, n->info))
+        return NULL;
+
+    return n;
+}
+
+int comp(DataType data1,DataType data1);
