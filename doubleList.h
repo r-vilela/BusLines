c6c11e2d27fcc4fc4a52f6d173dc70dd8c490813
R-Vilela <raulvilela29@gmail.com>
2025-11-07 17:27:48 -0400

    feat(doubleList): add documentation and Node Struct

--- doubleList.h
+++ doubleList.h
@@ -3,16 +3,23 @@
 
 /**
  * @defgroup DoubleList
- * @brief Lista duplamente encadeada circular
+ * @brief Lista duplamente encadeada circular.
+ * @warning Para utilizar, deve declarar os dados de DataType e a funcao comp, que sera usada para comparar os DataTypes.
  *
  */
 
 
 typedef struct data_type DataType;
 
+typedef struct node{
+    DataType info;
+    struct node *next;
+    struct node *prev;
+}Node;
+
 typedef struct{
-    DataType *head;
-    DataType *tail;
+    Node *head;
+    Node *tail;
 }DoubleList;
 
 /**
@@ -22,31 +29,37 @@ typedef struct{
  */
 
 /**
- * @brief Cria a lista
- * @param Recebe o ponteiro de uma DoubleList
+ * @brief Cria a lista.
+ * @param Recebe o ponteiro de uma DoubleList.
  */
 void create(DoubleList *list);
 
 /**
- * @brief Insere um elemento na lista
- * @param Recebe como parametro um ponteiro de uma DoubleList e o DataType para ser inserido na lista
- * @return retorna 1 se conseguir inserir ou 0 caso nao consiga
+ * @brief Insere um elemento na lista.
+ * @param Recebe como parametro um ponteiro de uma DoubleList e o DataType para ser inserido na lista.
+ * @return retorna 1 se conseguir inserir ou 0 caso nao consiga.
  */
 int insert(DoubleList *list, DataType data);
 
 /**
- * @brief Remove um elemento da lista
- * @param Recebe como parametro um ponteiro de uma DoubleLIst e o DataTyp que deseja remover da lista
- * @return retorna 1 se conseguir remver ou 0 casa nao consiga
+ * @brief Remove um elemento da lista.
+ * @param Recebe como parametro um ponteiro de uma DoubleLIst e o DataTyp que deseja remover da lista.
+ * @return retorna 1 se conseguir remver ou 0 casa nao consiga.
  */
 int dell(DoubleList *list, DataType data);
 
 /**
- * @brief Procurar elemento na lista
- * @param Recebe como parametro um ponteiro de uma DoubleLIst e o DataTyp que deseja procurar na lista
- * @return Retorna um ponteiro constante do elemento achado, evitando que o conteudo seja alterado, ou NULL, caso o elemento nao seja encontrado
+ * @brief Procurar elemento na lista.
+ * @param Recebe como parametro um ponteiro de uma DoubleLIst e o DataTyp que deseja procurar na lista.
+ * @return Retorna um ponteiro constante do elemento achado, evitando que o conteudo seja alterado, ou NULL, caso o elemento nao seja encontrado.
  */
 DataType const * search(DoubleList *list, DataType data);
+
+/**
+ * @brief Compara elementos na lista
+ * @warning esse funcao sera usada em nas operacoes da lista! Eh importante defini-la antes de usar.
+ */
+int comp(DataType data1,DataType data1);
 
 /** @} */
 
