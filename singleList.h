954ba40b4b3e173516d1d286a87401977961b945
TUF_garoa <raulvilela29@gmail.com>
2025-11-18 19:50:35 -0400

    feat(SingleList): define operations

--- singleList.h
+++ singleList.h
@@ -1,5 +1,6 @@
 // Lista Simplesmente Encadeada Dinamica
 
+#include <limits>
 typedef struct data Data;
 
 typedef struct node{
@@ -12,4 +13,14 @@ typedef struct{
     int size;
 }SingleList;
 
+void create(SingleList *list);
 
+int add(SingleList *list, Data *data);
+
+int deleteData(SingleList, Data *data);
+
+void show(SingleList *list);
+
+void showData(Data *data);
+
+void destroy(SingleList *list);
