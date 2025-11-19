75c3ffd5129ed668cc9d722843de2035506e1554
TUF_garoa <raulvilela29@gmail.com>
2025-11-18 21:29:14 -0400

    debuging

--- main.c
+++ main.c
@@ -1,75 +1,9 @@
 #include "bus.h"
-#include "doubleList.h"
 #include <stdio.h>
 
-typedef struct data_type {
-  int value;
-} BusLines;
-
-int comp(BusLines *data1, BusLines *data2) {
-  if (data1->value == data2->value)
-    return 1;
-  return 0;
-}
-
-void showData(DataType *data) { printf("%d - ", data->value); }
+// DoubleList
 
 int main() {
 
-  DoubleList L;
-
-  create(&L);
-
-  BusLines b1;
-  b1.value = 1;
-  BusLines b2;
-  b2.value = 2;
-  BusLines b3;
-  b3.value = 3;
-  BusLines b4;
-  b4.value = 4;
-
-  // Adicionando
-  add(&L, &b1);
-  add(&L, &b2);
-  add(&L, &b3);
-  add(&L, &b4);
-
-  show(&L);
-
-  // Removendo
-  removeData(&L, &b2);
-  printf("removeu 2\n");
-  show(&L);
-  printf("\n\n");
-
-  removeData(&L, &b3);
-  printf("removeu 3\n");
-  show(&L);
-  printf("\n\n");
-
-  // Procurando
-  printf("Procurando por %d\n", b4.value);
-  const DataType *d = search(&L, &b4);
-
-  if(d == NULL)
-      printf("Nao achou\n");
-  else
-      printf("Achou %d\n", d->value);
-
-  BusLines b5;
-  b5.value = 5;
-
-  printf("Procurando por %d\n", b5.value);
-  const DataType *d2 = search(&L, &b5);
-
-  if(d2 == NULL)
-      printf("Nao achou\n");
-  else
-      printf("Achou %d\n", d2->value);
-
-  destroy(&L);
-
-  show(&L);
   return 0;
 }
