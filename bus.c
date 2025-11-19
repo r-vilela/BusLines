ab4674cd56af8643086ab88c803a68d89c4c83f0
TUF_garoa <raulvilela29@gmail.com>
2025-11-18 21:22:37 -0400

    feat(bus): implement mandatory operations from double and single list and add line

--- bus.c
+++ bus.c
@@ -1,8 +1,40 @@
 #include "bus.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct data_type{
-    int id;
-    char name;
-    int arriveMin;
-    int leaveMin;
-}Lines;
+// SingleList
+
+int compS(Line *data1, Line *data2) {
+  return (data1->lineNum == data2->lineNum);
+}
+
+void showDataS(Data *data) {
+  printf("\t%d - %s\n", data->lineNum, data->lineName);
+}
+
+// DoubleList
+
+int comp(Stop *data1, Stop *data2) {
+  char *n1 = data1->stopName, *n2 = data2->stopName;
+  for (; n1 != NULL; n1++, n2++)
+    if (n1 != n2)
+      return 0;
+
+  return 1;
+}
+void showData(DataType *data) {
+  printf("\t%s - Arrives at %d and leaves at %d\n", data->stopName,
+         data->arrival, data->leave);
+}
+
+int addLine(Bus *bus, int lineNum, char lineName[15]){
+    Line *l = (Line *) malloc(sizeof(Line));
+    if(l == NULL)
+        return -1;
+    strcpy(l->lineName, lineName);
+    l->lineNum = lineNum;
+    l->stop = NULL;
+
+    return addS(bus->Lines, l);
+}
--- bus.h
+++ bus.h
@@ -1,6 +1,25 @@
 #ifndef BUS_H
 #define BUS_H
+#include "singleList.h"
 #include "doubleList.h"
+
+typedef struct data {
+  int lineNum;
+  char lineName[15];
+  DoubleList *stop;
+} Line;
+
+typedef struct data_type {
+  char stopName[15];
+  int arrival; // Armazenado em minutos
+  int leave;   // Armazenado em minutos
+} Stop;
+
+typedef struct{
+    SingleList *Lines;
+}Bus;
+
+int addLine(Bus *bus, int lineNum, char lineName[15]);
 
 #endif
 
